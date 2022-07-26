0a1244a69946a972dc6684f39085a03f46a855a8
Sebastian Gilits <sep.gil@gmail.com>
2022-07-26 23:05:19 +0200

    Change defaults for synth UI

--- include/ElectSynthUI.h
+++ include/ElectSynthUI.h
@@ -39,7 +39,7 @@ protected:
     void adsrEncoderEvent(int encoder, bool moved_left);
     void adsrEncoderButtonEvent(int encoder, bool pressed);
 
-    uint8_t page = 2;
+    uint8_t page = 0;
 
     ElectSynth *synth;
     AudioAnalyzeOscilloscope *scope;
@@ -51,8 +51,8 @@ protected:
     uint8_t vcoMixMod = 0;
     float vcoMixModes[3] = {0.1f, 0.01f, 0.001f};
 
-    uint8_t vcfFreqMode = 1;
-    uint8_t vcfQMode = 1;
+    uint8_t vcfFreqMode = 0;
+    uint8_t vcfQMode = 0;
     float vcfFreqModes[3] = {50.f, 10.0f, 1.0f};
     float vcfQModes[3] = {0.5f, 0.1f, 0.01f};
     int vcfRadius[3] = {5, 3, 1};
